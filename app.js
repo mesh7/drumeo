06c9338c5591d67546ad49567987d4d416d9cde7
Shubham Meshram <meshram.shubham27@gmail.com>
2023-08-05 22:51:56 +0530

    added styling

---
+++ app.js
@@ -0,0 +1,13 @@
+let drumkey = "hello";
+
+function drumpadSound(dumkey) {
+  switch (dumkey) {
+    case 'drum':
+      console.log('drum')
+      break;
+    case 'hi_hat':
+        console.log('hi_hat')
+      break;
+    default:
+  }
+}
